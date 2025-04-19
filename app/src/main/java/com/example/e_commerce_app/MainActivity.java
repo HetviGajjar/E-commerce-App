6c710f7890fad2b09414b4d86a3f7443f91b27cd
HetviGajjar <hetvigajjar585@gmail.com>
2025-04-19 11:52:57 +0530

    E-Commerce-030

---
+++ app/src/main/java/com/example/e_commerce_app/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.e_commerce_app;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+public class MainActivity extends AppCompatActivity {
+
+    int[] images = {R.drawable.img1, R.drawable.img2, R.drawable.img3};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager2 viewPager = findViewById(R.id.viewPager);
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+
+        ImageAdapter adapter = new ImageAdapter(images);
+        viewPager.setAdapter(adapter);
+
+        new TabLayoutMediator(tabLayout, viewPager, (tab, position) -> {
+            // dots only, no text
+        }).attach();
+    }
+}
